71191d5fdbaa8289dfd90c384c19b1ddcd12e5d7
vpnineh <fffarbod@bk.ru>
2026-01-30 05:53:17 +0330

    Update run.py

--- run.py
+++ run.py
@@ -2,12 +2,13 @@ import requests
 import base64
 
 config_urls = [
-    "https://github.com/Aa64n/Aa64n-/raw/refs/heads/main/b",
+    #"https://github.com/Aa64n/Aa64n-/raw/refs/heads/main/b",
     "https://github.com/Aa64n/Aa64n-/raw/refs/heads/main/backk%20up%20new",
     "https://github.com/Aa64n/Aa64n-/raw/refs/heads/main/family",
-    "https://github.com/Aa64n/Aa64n-/raw/refs/heads/main/freind",
-    "https://github.com/Aa64n/Aa64n-/raw/refs/heads/main/wir",
-    "https://raw.githubusercontent.com/Pawdroid/Free-servers/refs/heads/main/sub"
+    "https://raw.githubusercontent.com/Aa64n/Aa64n-/refs/heads/main/new",
+    #"https://github.com/Aa64n/Aa64n-/raw/refs/heads/main/freind",
+    #"https://github.com/Aa64n/Aa64n-/raw/refs/heads/main/wir",
+    #"https://raw.githubusercontent.com/Pawdroid/Free-servers/refs/heads/main/sub"
 ]
 
 drive_url = "https://drive.google.com/uc?export=download&id=1-EopH8hKLwaRJ3kxm3-40x4CZQ3prAzP"
